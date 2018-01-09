5059a8092d6c52320c7a674681f993ef7a425313
tuozhaobing <1044176601@qq.com>
2018-01-09 23:43:27 +0800

    add bottom navigation view for main page

--- app/src/main/java/com/example/code/screenrecorder/MainActivity.java
+++ app/src/main/java/com/example/code/screenrecorder/MainActivity.java
@@ -1,20 +1,24 @@
 package com.example.code.screenrecorder;
 
+import android.support.design.widget.BottomNavigationView;
 import com.example.code.screenrecorder.common.BaseActivity;
+import com.example.code.screenrecorder.common.helper.BottomNavigationViewHelper;
 
 public class MainActivity extends BaseActivity {
+    private BottomNavigationView mBottomNavigationView;
     @Override
     public void setContentView() {
-
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public void initView() {
-
+        mBottomNavigationView = findViewById(R.id.main_bottom_navigation_view);
     }
 
     @Override
     public void initData() {
-
+        mBottomNavigationView.setItemIconTintList(null);
+        BottomNavigationViewHelper.disableShiftMode(mBottomNavigationView);
     }
 }
