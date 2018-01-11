b5fcbe45910a869b6339f270de9b9368056e80b8
tuozhaobing <1044176601@qq.com>
2018-01-11 15:57:59 +0800

    add tab res

--- app/src/main/java/com/example/code/screenrecorder/MainActivity.java
+++ app/src/main/java/com/example/code/screenrecorder/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.code.screenrecorder;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTabHost;
+
 public class MainActivity extends BaseActivity {
+
+  @NonNull
+  private FragmentTabHost mFragmentTabHost;
 
   @Override
   void setContentView() {
-
+    setContentView(R.layout.activity_main);
   }
 
   @Override
   void initView() {
-
+    mFragmentTabHost = findViewById(R.id.main_tab_host);
+    mFragmentTabHost.setup(this,getSupportFragmentManager());
   }
 
   @Override
   void initData() {
+    initTabs();
+  }
+
+  private void initTabs(){
 
   }
 }
